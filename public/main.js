7ad873ed5b434dab9fc063bf8c27c649b64013cb
iqbalfachry19 <ifachry@rocketmail.com>
2021-10-24 22:02:51 +0700

    first

---
+++ public/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.querySelector(".button").disabled = true;
+
+  document.querySelector(".input").addEventListener("change", stateHandle);
+});
+function stateHandle() {
+  if (document.querySelector(".input").value === "") {
+    document.querySelector(".button").disabled = true;
+    document.querySelector(".button").classList.remove("btn_join");
+    document.querySelector(".button").classList.add("btn_disabled");
+  } else {
+    document.querySelector(".button").disabled = false;
+    document.querySelector(".button").classList.remove("btn_disabled");
+    document.querySelector(".button").classList.add("btn_join");
+  }
+}
